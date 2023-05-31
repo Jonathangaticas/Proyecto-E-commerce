9bb9fcd8f33a2c114fecb5aef24b481bbcf366a8
Jonathan Gatica <jonathangaticas@hotmail.com>
2023-05-31 12:56:45 -0400

    Act-pag-ppal

--- src/components/Navbar/Secciones/Secciones.jsx
+++ src/components/Navbar/Secciones/Secciones.jsx
@@ -8,10 +8,10 @@ const Secciones = () => {
         <ul className="navbar-nav me-auto">
           <li className="nav-item"></li>
           <li className="nav-item dropdown">
-            <Link className="link nav-link dropdown-toggle" data-bs-toggle="dropdown" element="#" role="button" aria-haspopup="true" aria-expanded="false">Productos por Categoría</Link>
-          <div className="dropdown-menu">
-            <Categorias/>
-          </div>
+            <Link className="link nav-link dropdown-toggle p-5" data-bs-toggle="dropdown" element="#" role="button" aria-haspopup="true" aria-expanded="false">Productos por Categoría</Link>
+              <div className="dropdown-menu">
+                <Categorias/>
+              </div>
         </li>
       </ul>
     );
